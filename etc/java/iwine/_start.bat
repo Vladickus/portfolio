7b921e121bbeb62a5d6d63d374e4ed73ce3ae242
Vladickus <53038703+Vladickus@users.noreply.github.com>
2019-11-02 16:48:58 +0700

    Add files via upload

---
+++ etc/java/iwine/_start.bat
@@ -0,0 +1,11 @@
+@echo off
+
+rem C:\JBuilderX\jdk1.4\bin\appletviewer.exe show_Omnes.html
+appletviewer.exe show_Omnes.html
+
+
+rem java -classpath .;applets_iWine.jar iwine.components.demo.Application
+rem C:\JBuilderX\jdk1.4\bin\appletviewer.exe AppDevice.html
+rem C:\JBuilderX\jdk1.4\bin\appletviewer.exe AppSettings.html
+rem C:\JBuilderX\jdk1.4\bin\appletviewer.exe AppServer.html
+êóü C:\JBuilderX\jdk1.4\bin\appletviewer.exe AppGraphine.html
